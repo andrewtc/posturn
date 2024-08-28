fda3c4e9368cb4f79e969e098bd49619b090e992
Andrew Thomas Christensen <andrew@andrewtc.com>
2024-08-28 15:32:31 -0500

    Fix typo in `net` example
    The call to `draw_text_ex` was **not** using `TITLE_TEXT` in the `text` parameter, allowing it to be changed independently of the text used in the `get_text_center` calculation. This has been fixed.

--- examples/net/main.rs
+++ examples/net/main.rs
@@ -17,7 +17,7 @@ async fn main() {
 
       let title_text_center = get_text_center(TITLE_TEXT, None, title_text_params.font_size, title_text_params.font_scale, title_text_params.rotation);
       let title_text_pos = (0.5f32 * Vec2::from(screen_size())) - title_text_center;
-      draw_text_ex("Out West!", title_text_pos.x, title_text_pos.y, title_text_params);
+      draw_text_ex(TITLE_TEXT, title_text_pos.x, title_text_pos.y, title_text_params);
 
       next_frame().await;
    }
